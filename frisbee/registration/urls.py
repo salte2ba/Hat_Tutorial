849f87e98cc7213e025734f93267b47649396405
salte2ba <salte2ba@cmich.edu>
2014-11-05 09:06:27 -0500

    got the nongeneric views working!

--- frisbee/registration/urls.py
+++ frisbee/registration/urls.py
@@ -3,4 +3,6 @@ from registration import views
 
 urlpatterns = [
 	url(r'^$', views.home, name = 'home'),
+	url(r'^signup/$', views.signup, name = 'signup'),
+	url(r'^thanks/$', views.thanks, name = 'thanks'),
 ]
--- frisbee/registration/views.py
+++ frisbee/registration/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def home(request):
 	return HttpResponse("Hello and welcome to the Hat Tournament signup!")
+def signup (request):
+	return HttpResponse("This is where you will sign up")
+def thanks (request):
+	return HttpResponse("Thank you for signing up!")	
+
